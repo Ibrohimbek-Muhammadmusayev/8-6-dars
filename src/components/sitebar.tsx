0ffe8893054754e8eecf59fb254e1c6595c8a87e
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2024-07-08 14:34:45 +0500

    yangilandi

--- src/components/sitebar.tsx
+++ src/components/sitebar.tsx
@@ -12,7 +12,7 @@ export default function Sitebar() {
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 flex flex-col gap-[10px] text-base-content min-h-full w-80 pr-4" data-theme='dark'>
                     <div className="w-[320px] h-[100px] flex mr-[10px] justify-center items-center">
-                        <h1 className="font-bold text-[35px] mx-auto">Admin Deshboard</h1>
+                        <h1 className="font-bold text-[35px] mx-auto">Admin Dashboard</h1>
                     </div>
                     <li className='max-w-[300px] text-[20px]'><NavLink to={'/'}>Dashboard</NavLink></li>
                     <li className='max-w-[300px] text-[20px]'><NavLink to={'/products'}>Products</NavLink></li>
